217aae6025f803f42bc861f2c7ba3a9a5be98b5d
Alen Thomas <techiealen@gmail.com>
2026-04-18 00:59:48 +0530

    Remove arrow from Get Started CTA button, bump SW cache to v19

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
      - Other assets (images/fonts): cache-first with network fill
    ============================================================ */
 
-const CACHE_NAME = 'hexago-v18';
+const CACHE_NAME = 'hexago-v19';
 const OFFLINE_PAGE = '/404.html';
 const NAVIGATION_TIMEOUT_MS = 4000;
 
